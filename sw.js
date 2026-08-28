493d8f1250b8943403fbf8394a1a32c6a82fc54e
Cesco <320647740+cescodrop-dot@users.noreply.github.com>
2026-08-28 07:19:44 +0200

    Add service worker for offline app shell

---
+++ sw.js
@@ -0,0 +1,76 @@
+const CACHE_VERSION = 'cescospot-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './styles/app.css',
+  './js/manifest.js',
+  './js/data-safety.js',
+  './js/storage.js',
+  './js/app.js',
+  './vendor/suncalc.js',
+  './vendor/fontawesome/css/all.min.css',
+  './vendor/fontawesome/webfonts/fa-solid-900.woff2',
+  './vendor/fontawesome/webfonts/fa-regular-400.woff2',
+  './map.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_VERSION).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const requestUrl = new URL(event.request.url);
+  const isNavigation = event.request.mode === 'navigate';
+  const isSameOrigin = requestUrl.origin === self.location.origin;
+
+  if (isNavigation) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_VERSION).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  if (isSameOrigin) {
+    event.respondWith(
+      caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_VERSION).then(cache => cache.put(event.request, copy));
+        return response;
+      }))
+    );
+    return;
+  }
+
+  if (requestUrl.hostname === 'unpkg.com') {
+    event.respondWith(
+      caches.match(event.request).then(cached => {
+        const network = fetch(event.request).then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_VERSION).then(cache => cache.put(event.request, copy));
+          return response;
+        });
+        return cached || network;
+      })
+    );
+  }
+});
